9a5afd1c2262879bcb427f64e7866d6a3e597be5
Fabien <ftrestour@gmail.com>
2024-01-19 18:03:03 +0100

    Setup tailwind

--- src/server.tsx
+++ src/server.tsx
@@ -3,10 +3,12 @@ import { html } from "@elysiajs/html";
 
 new Elysia()
   .use(html())
+  .use(staticPlugin())
   .get("/", () => (
     <html lang="en">
       <head>
         <title>Hello World</title>
+        <link href="./public/output.css" rel="stylesheet"></link>
       </head>
       <body>
         <h1>Hello World</h1>
